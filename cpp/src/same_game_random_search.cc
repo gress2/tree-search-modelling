69f6d0ad8d0c8ac3992b72642f921de8efaf128f
Collin Gress <gress126@gmail.com>
2018-11-16 11:48:03 -0600

    add missing file

---
+++ cpp/src/same_game_random_search.cc
@@ -0,0 +1,12 @@
+#include <cstdlib>
+#include <ctime>
+#include <fstream>
+
+#include "game.hpp"
+#include "search.hpp"
+
+int main() {
+  std::srand(std::time(0));
+  std::ofstream out_file("depths");
+  random_search<same_game>(1e5, out_file);
+}
